628dcbaab0d11e658b6adb8446df747602f996c1
Oyakoya Curtis <curtsoyaks@gmail.com>
2021-08-03 16:35:35 +0100

    updated navbar

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -11,7 +11,7 @@ const LandingPage: FC = () => {
             <Flex mt="5rem" flexDir={{ base: 'column', md: 'row' }} maxWidth="990px" m="0 auto" justifyContent={{ base: 'space-around', md: 'space-between' }} alignItems={{ base: 'flex-start', md: 'center' }}>
             <Box  mr={{base:"1rem", md:"1rem"}} ml={{base:"1rem", md:"1rem"}}>
                 <Text as="p" opacity="0.8" mb={{base:"0.7rem",md:"2rem"}} mt="2.7rem">OPEN SEASON</Text>
-                <Text as="h1" fontSize={{base:"2rem",md:"4rem"}} fontWeight="bold" mb={{base:"0.7rem",md:"2rem"}}>Weather <br/>
+                <Text as="h1" fontSize={{base:"3rem",md:"4rem"}} fontWeight="bold" mb={{base:"0.7rem",md:"2rem"}}>Weather <br/>
                 Forecast</Text>
                 <Link to="/weather">
                 <Button bg="#f3f" color="#fff"  padding="1.2rem;" boxShadow="0px 5px 5px rgb(75 93 104 / 10%);" borderRadius="2.5rem" mt={{base:"1.5rem",md:"3rem"}} _hover={{ bg: "#f3f" }}>Discover</Button>
