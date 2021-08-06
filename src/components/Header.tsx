901725ec56a97677dc6c824f55864ee48fff3504
Oyakoya Curtis <curtsoyaks@gmail.com>
2021-08-06 21:54:22 +0100

    text update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header: FC = ()=>{
 <Box maxWidth="990px" m="0 auto" pr={{base:"1rem", md:"0rem"}} pl={{base:"1rem", md:"0rem"}}> 
     <Flex  justifyContent="space-between" alignItems="center" mt="1.5rem">
     <Link to="/">
-        <Text fontSize="1.8rem" fontWeight="bold" color="#fff">W <Text as="span" color="#f2f">T</Text> <Text as="span" color="#55a7ff">A</Text> </Text>
+        <Text fontSize="1.8rem" textShadow= "-3px 1px 2px rgba(0, 0, 0, 0.53)" fontWeight="bold" color="#fff">W <Text as="span" color="#f2f">T</Text> <Text as="span" color="#55a7ff">A</Text> </Text>
         </Link>
         <Link to="/weather">
         <Button bg="#f3f" color="#fff"  padding="1.2rem" boxShadow="0 1rem 3rem -1rem #484848" borderRadius="3rem" height="45px" width="130px" _hover={{ bg: "#f3f" }}>Discover</Button>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -10,8 +10,8 @@ const LandingPage: FC = () => {
             <Header/>
             <Flex mt="9rem" flexDir={{ base: 'column', md: 'row' }} maxWidth="990px" m="0 auto" justifyContent={{ base: 'space-around', md: 'space-between' }} alignItems={{ base: 'flex-start', md: 'center' }}>
             <Box  mr={{base:"1rem", md:"1rem"}} ml={{base:"1rem", md:"1rem"}} mb="2rem">
-                <Text as="p" opacity="0.8" mb={{base:"0.7rem",md:"2rem"}} mt="2.7rem">OPEN SEASON</Text>
-                <Text as="h1" fontSize={{base:"3.4rem",md:"4rem"}} fontWeight="bold" mb={{base:"0.7rem",md:"2rem"}}>Weather <br/>
+                <Text as="p" textShadow= "-3px 1px 2px rgba(0, 0, 0, 0.53)" opacity="0.95" mb={{base:"0.7rem",md:"2rem"}} fontWeight="bold" mt="2.7rem">OPEN SEASON</Text>
+                <Text as="h1" textShadow= "-3px 1px 2px rgba(0, 0, 0, 0.53)" fontSize={{base:"3.4rem",md:"4rem"}} fontWeight="bold" mb={{base:"0.7rem",md:"2rem"}}>Weather <br/>
                 Forecast</Text>
                 <Link to="/weather">
                 <Button bg="#f3f" color="#fff"  padding="1.2rem;" height="45px" width="130px"  boxShadow="0 1rem 3rem -1rem #484848" borderRadius="2.5rem" mt={{base:"1.5rem",md:"3rem"}} _hover={{ bg: "#f3f" }}>Discover</Button>
